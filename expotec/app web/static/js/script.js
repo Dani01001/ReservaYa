80a625bc6ab148147fbdb6607fb5651463cf4482
toshipedrozo <toshinicolaspedrozo@gmail.com>
2025-07-28 08:10:09 -0300

    Add files via upload

---
+++ expotec/app web/static/js/script.js
@@ -0,0 +1,11 @@
+function abrirVentanaEmergente(url) {
+    const ancho = 800; // Ancho de la ventana
+    const alto = 900;  // Alto de la ventana
+
+    // Obtener dimensiones de la pantalla
+    const left = (window.innerWidth - ancho) / 2;
+    const top = (window.innerHeight - alto) / 2;
+
+    // Crear la ventana emergente centrada
+    window.open(url, 'VentanaEmergente', `width=${ancho},height=${alto},top=${top},left=${left}`);
+}
