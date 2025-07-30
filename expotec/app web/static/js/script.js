7c85eb1c9ee9e07adf6b94afb306d79e7680897b
toshipedrozo <toshinicolaspedrozo@gmail.com>
2025-07-30 08:14:34 -0300

    configuramos los formularios y cambiamos los colores de dichos formularios, estan prestados a cambios los respectivos colores pq parecen kk

--- expotec/app web/static/js/script.js
+++ expotec/app web/static/js/script.js
@@ -1,6 +1,6 @@
 function abrirVentanaEmergente(url) {
-    const ancho = 800; // Ancho de la ventana
-    const alto = 900;  // Alto de la ventana
+    const ancho = 700; // Ancho de la ventana
+    const alto = 550;  // Alto de la ventana
 
     // Obtener dimensiones de la pantalla
     const left = (window.innerWidth - ancho) / 2;
@@ -10,7 +10,7 @@ function abrirVentanaEmergente(url) {
     window.open(url, 'VentanaEmergente', `width=${ancho},height=${alto},top=${top},left=${left}`);
 }
 
-fetch('http://127.0.0.1:8080/api/reservas/')
+fetch('http://120.0.0.1:8000/api/reservas/')
     .then(res => res.json())
     .then(data => {
         console.log("Datos desde la api", data);
