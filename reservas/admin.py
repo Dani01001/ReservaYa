7160721190bfb6db81f8f71c5bc7c33bbe0a8c1e
Dani <157851656+Dani01001@users.noreply.github.com>
2025-08-19 20:17:28 -0300

    Revert "Pagina"

--- reservas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Reserva
-
-admin.site.register(Reserva)
-
-# Register your models here.
--- reservas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.crear_reserva, name='crear_reserva'),
-    path('listar/', views.listar_reservas, name='listar_reservas'),
-    path('api/', include('reservas.api_urls')),
-]
-
