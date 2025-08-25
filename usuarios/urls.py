209e3b7d46803e5554983d9c3bf5fbafd1e76c62
Sebas <tareasinforodrigo@gmail.com>
2025-08-25 17:13:01 -0300

    se logro

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.lista_usuarios, name='lista_usuarios'),
+    path("registrar", views.registrar_usuario, name="registrar_usuario"),
     path("registro/", views.registrar_usuario, name="registro"),
-    path("login/", views.iniciar_sesion, name="login"),
-    path("logout/", views.cerrar_sesion, name="logout"),
+    path("login/", views.iniciar_sesion, name="iniciar_sesion"),
+    path("logout/", views.cerrar_sesion, name="cerrar_sesion"),
 ]
