d75dacbfcc87ab5b0d9ed803c573a1ed4e2ea3cc
Sebas <tareasinforodrigo@gmail.com>
2025-09-08 10:30:49 -0300

    08-09-2025

--- JS/usuario.js
+++ JS/usuario.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/api/reservas/", {
+        const response = await fetch("http://192.168.170.96:8000/api/reservas/", {
             headers: {
                 "Authorization": "Token " + token
             }
